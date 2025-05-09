b0e686db3e5c40133f93d72adf3dc7822b3d461a
Chendong <songcd1212@gmail.com>
2025-05-09 05:56:37 +0000

    delete

--- AutoFormalization/IMO/data_load.py
+++
@@ -1,5 +0,0 @@
-from huggingface_hub import login
-login(token="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")
-from huggingface_hub import HfApi
-from datasets import load_dataset
-ds = load_dataset("AI-MO/GeometryLeanPrompt",)
