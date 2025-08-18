a4359426538363a3b278c741b72131eb5c98f28a
Chendong Song <songcd1212@gmail.com>
2025-08-18 09:23:23 +0000

    clean unecessary documents

--- IMO/Bench/Bench/High_School_level/6.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib
-import SystemE
-import LeanGeo
-namespace LeanGeo
-
-theorem day156 : ∀ (A B M N O P : Point) (AB NP OP : Line) (C: Circle),
-Diameter A B O C ∧ P.onCircle C ∧ O.isCentre C∧ ∠A:O:P = ∟ ∧ between A M O ∧ between N M P ∧ N.onCircle C ∧
-distinctPointsOnLine A B AB ∧ distinctPointsOnLine N P NP ∧ distinctPointsOnLine O P OP →    |(A─B)| * |(A─B)| = 2* |(P─M)| * |(N─P)| := by
-  sorry
